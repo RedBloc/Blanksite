2b8479754d0698272588f3f8e61e6223b6df462b
RedBloc <RedBloc@Reds-iMac.local>
2015-09-03 14:48:33 +0100

    Initial setup
    Just a blank site.

---
+++ components/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+$(".orgTab").click(function(e){
+e.preventDefault();
+	 $('.tabsMenu ul .selected').removeClass('selected');
+	 $(this).addClass('selected');
+	 
+		$(".tab01").slideToggle();
+		$(".tab02").slideUp();
+		$(".tab03").slideUp();
+		})
+
+$(".raiseTab").click(function(e){
+e.preventDefault();
+	 $('.tabsMenu ul .selected').removeClass('selected');
+ $(this).addClass('selected');
+ 
+		$(".tab02").slideToggle();
+		$(".tab01").slideUp();
+		$(".tab03").slideUp();
+		})
+
+$(".promoTab").click(function(e){
+e.preventDefault();
+	 $('.tabsMenu ul .selected').removeClass('selected');
+ $(this).addClass('selected');
+		$(".tab03").slideToggle();
+		$(".tab02").slideUp();
+		$(".tab01").slideUp();
+		})
+
+});
